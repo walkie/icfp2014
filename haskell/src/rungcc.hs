fa24aeb28494bd05fc7742be6f8f243a5cd2957f
Eric Walkingshaw <eric@walkingshaw.net>
2014-07-27 01:29:58 +0200

    GCC: add help flag.

--- haskell/src/rungcc.hs
+++ haskell/src/rungcc.hs
@@ -1,15 +1,17 @@
 
 module Main where
 
+import Control.Monad
 import System.Console.GetOpt
 import System.Environment
+import System.Exit
 
 import LahnParty.GCC
 
 
 -- ** Command line options
 
-data Opt = Symbolic | Trace
+data Opt = Symbolic | Trace | Help
   deriving (Eq,Show)
 
 opts =
@@ -17,16 +19,19 @@ opts =
       "program uses symbolic names rather than addresses"
   , Option ['t'] ["trace"]    (NoArg Trace)
       "print trace of machine states during execution"
+  , Option ['h'] ["help"]     (NoArg Help)
+      "print this help message and exit"
   ]
+
+usage :: String
+usage = usageInfo "Run a GCC program provided on STDIN." opts
 
 readOpts :: IO [Opt]
 readOpts = do
     args <- getArgs
     case getOpt Permute opts args of
       (os,[],[]) -> return os
-      (_ ,_ ,es) -> ioError (userError (concat es ++ usageInfo head opts))
-  where
-    head = "Run a GCC program provided on STDIN."
+      (_ ,_ ,es) -> ioError (userError (concat es ++ usage))
 
 
 -- ** Read program from STDIN
@@ -42,6 +47,7 @@ readProgram = getContents >>= return . program . read
 
 main = do
   os <- readOpts
+  when (elem Help os) (putStr usage >> exitSuccess)
   p  <- if elem Symbolic os
           then readAssembly >>= return . assemble
           else readProgram

7a012cab08effa7724f9c843bd4c4b12b48acb2f
Eric Walkingshaw <eric@walkingshaw.net>
2014-07-26 22:32:39 +0200

    GCC: pretty print execution state.

--- haskell/src/LahnParty/GCC/Pretty.hs
+++ haskell/src/LahnParty/GCC/Pretty.hs
@@ -7,6 +7,7 @@ import Data.List (intercalate)
 import LahnParty.GCC.Assembler
 import LahnParty.GCC.State
 import LahnParty.GCC.Syntax
+import LahnParty.GCC.Error
 
 
 prettyAssembly :: Assembly -> String
@@ -19,10 +20,53 @@ prettyAssembly p = unlines (map block p)
     inst i          = show i
 
 prettyProgram :: Program -> String
-prettyProgram p = (unlines . map line . assocs) p
+prettyProgram p = unlines (map (numberedLine k show) (assocs p))
+  where k = (length . show . snd . bounds) p
+
+prettyResult :: Either Error GCC -> String
+prettyResult = either show prettyGCC
+
+prettyStack :: (a -> String) -> Stack a -> String
+prettyStack f s = unlines (map (numberedLine k f) (zip [0..] s))
+  where k = length (show (length s))
+
+prettyStackD :: DataStack -> String
+prettyStackD = prettyStack prettyValue
+
+prettyStackC :: ControlStack -> String
+prettyStackC = prettyStack prettyControl
+
+prettyValue :: Value -> String
+prettyValue (Lit i)    = show i
+prettyValue (Pair a b) = "(" ++ prettyValue a ++ ", " ++ prettyValue b ++ ")"
+prettyValue (Clos f m) = show f ++ " " ++ prettyEnv 2 m
+
+prettyControl :: Control -> String
+prettyControl (Join     a) = "Join "   ++ show a
+prettyControl (Return   a) = "Return " ++ show a
+prettyControl (FramePtr e) = "FramePtr" ++ 
+                             if null e then " [Empty]" else prettyEnv 8 e
+prettyControl Stop         = "Stop"
+
+prettyFrame :: Int -> (Int,Frame) -> String
+prettyFrame k (n,f) = unlines $ padLeft k ("Frame " ++ show n) : go f
   where
-    k = (length . show . snd . bounds) p
-    line (addr, inst) = padLeft k (show addr) ++ ": " ++ show inst
+    go (Dummy m)   = ["  [Dummy " ++ show m ++ "]"]
+    go (Values vs) = map (numberedLine (k + k' + 2) prettyValue) (zip [0..] vs)
+      where k' = length (show (length vs))
+
+prettyEnv :: Int -> Env -> String
+prettyEnv k = unlines . map (prettyFrame k) . zip [0..]
+
+prettyGCC :: GCC -> String
+prettyGCC (GCC pc ds cs env) = unlines 
+  ["PC: " ++ show pc, "",
+   "== Data Stack ==",    prettyStackD ds,
+   "== Control Stack ==", prettyStackC cs,
+   "== Environment ==",   prettyEnv 0 env]
+
+numberedLine :: Int -> (a -> String) -> (Int, a) -> String
+numberedLine k f (i,a) = padLeft k (show i) ++ ": " ++ f a
 
 padLeft :: Int -> String -> String
 padLeft k s = replicate (k - length s) ' ' ++ s
